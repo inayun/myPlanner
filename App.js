23108412017fa11a0b5f442474aa941a621b1e87
inayun <ina-yun@hanmail.net>
2020-04-27 17:04:21 +0900

    make a Input section and ToDo components

--- App.js
+++ App.js
@@ -1,25 +1,95 @@
 import React from 'react';
-import { StyleSheet, Text, View, StatusBar } from 'react-native';
+import { StyleSheet
+        , Text
+        , View
+        , StatusBar
+        , TextInput
+        , Dimensions
+        , Platform
+        , ScrollView
+         } from 'react-native';
 
-export default function App() {
+import ToDo from "./ToDo.js";
+
+const{ height, width } = Dimensions.get("window");
+
+export default class App extends React.Component {
+  state = {
+    newToDo: ""
+  };
+
+  render() {
+
+  const { newToDo } = this.state;
   return (
     <View style={styles.container}>
       <StatusBar barStyle = "light-content" />
       <Text style={styles.title}>myPlanner</Text>
+      <View style={styles.card}>
+        <TextInput 
+        style={styles.input} 
+        placeholder={"New To Do"} 
+        value={newToDo}
+        onChangeText={this._controlNewToDo} 
+        placeholderTextColor={"#999"}
+        returnKeyType = {"done"}
+        autoCorrect={false}
+        />
+        <ScrollView>
+          <ToDo />
+        </ScrollView> 
+      </View> 
     </View>
   );
+ }
+  _controlNewToDo = text => {
+    this.setState({
+      newToDo: text
+    });
+  }
+
 }
 
 const styles = StyleSheet.create({
   container: {
+
     flex: 1,
-    backgroundColor: '#F23657',
+    backgroundColor: 'gray',
     alignItems: 'center',
   },
   title: {
-    color: "white",
+    color: "black",
     fontSize : 30,
     marginTop: 50,
+    marginBottom : 50,
     fontWeight: "400"
+  },
+  card: {
+    backgroundColor : "white",
+    flew : 1,
+    width: width - 25,
+    hegith : height - 5,
+    borderTopLeftRadius : 10,
+    borderTopRightRadius : 10,
+    ...Platform.select ({
+      ios: {
+        shadowColor : "rgb(50,50,50)",
+        shadowOpactity: 0.5,
+        shadowRadius:5,
+        shawdowOffset: {
+          height :-1,
+          width:0
+        }
+      },
+      android: {
+        elevation: 3
+      }
+    })
+  },
+  input: {
+    padding : 18,
+    borderBottomColor : "#bbb",
+    borderBottomWidth: 1,
+    fontSize: 15
   }
 });
---
+++ ToDo.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { StyleSheet
+        , Text
+        , View
+        , TouchableOpacity
+         } from 'react-native';
+
+export default class ToDo extends React.Component{
+  render() {
+    return(
+    <View>
+      <Text>Hello, Nice to meet you </Text>
+    </View>
+    );
+  }
+
+
+}
+
+const styles = StyleSheet.create({
+
+})
